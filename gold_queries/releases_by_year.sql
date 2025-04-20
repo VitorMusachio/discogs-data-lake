705c031c3a9992094627d83ed7de38d7e991f009
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2025-04-20 15:39:22 -0300

    Delete gold_queries directory

--- gold_queries/releases_by_year.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    dt_release_date AS year, 
-    COUNT(*) AS total_releases
-FROM
-    parquet.`s3a://silver/silver/releases/`
-GROUP BY
-    dt_release_date
-ORDER BY
-    year
--- gold_queries/top_artists.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id_artist,
-    name,
-    followers
-FROM
-    parquet.`s3a://silver/silver/artists/`
-ORDER BY
-    followers DESC
-LIMIT 100
